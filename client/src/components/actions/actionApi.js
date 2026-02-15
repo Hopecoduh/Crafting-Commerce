f4791923e841d20b24b3ef14ef8f745a80a6e6cf
Mac <33965646+SodaDakoda@users.noreply.github.com>
2026-02-14 20:05:32 -0800

    fixed action.api url

--- client/src/components/actions/actionApi.js
+++ client/src/components/actions/actionApi.js
@@ -1,10 +1,12 @@
 // client/src/components/actions/actionApi.js
 import { getToken } from "../../api.js";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "";
+
 export async function postAction(url, body) {
   const token = getToken();
 
-  const res = await fetch(url, {
+  const res = await fetch(`${BASE_URL}${url}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
